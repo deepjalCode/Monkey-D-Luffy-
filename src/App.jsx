b455575043c121f1b952160c8d750b4e3a2b32e9
deepjalCode <deepjalofficial@gmail.com>
2026-01-06 19:42:32 +0530

    Props passing details

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import Navbar from './components/Navbar'
 import LeftSection from './components/LeftSection'
 
 const App = () => {
+  const onePieceCharacters = [
+  {
+    name: "Roronoa Zoro",
+    url: "https://onepiece.fandom.com/wiki/Roronoa_Zoro",
+    description: "The combatant of the Straw Hat Pirates and a master swordsman who uses the Three-Sword Style. Zoro’s goal is to become the world’s greatest swordsman, and he is known for extreme discipline, loyalty, and endurance."
+  },
+  {
+    name: "Usopp",
+    url: "https://onepiece.fandom.com/wiki/Usopp",
+    description: "The sniper of the Straw Hat Pirates. Initially a coward and compulsive liar, Usopp gradually develops courage and confidence, using his intelligence, marksmanship, and inventions to support the crew."
+  }
+];
+
   
   return (
-    <div className='h-screen w-full bg-red-800'>
+    <div className='h-screen w-full bg-red-600'>
       <Navbar />
+      <div className='px-12'>
+        <LeftSection onePieceCharacters={onePieceCharacters}/>
+      </div>
       
     </div>
     
--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const LeftSection = () => {
   return (
-    <div>LeftSection</div>
+    <div className='bg-pink-200 flex px-1 py-1 rounded-tl-4xl rounded-bl-xl w-[35%] rounded-2xl'>
+      <img 
+        className='h-26 w-30 object-cover rounded-tr-xl rounded-bl-xl rounded-tl-4xl rounded-br-4xl '
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqU3BhraKoJ5i52OV1C5k0z-DEG8xhi-mmcw&s" alt="" />
+      <div className='flex flex-col items-start justify-center  px-2'>
+        <h3 className='text-white text-xl  font-bold mb-1'>Color Code</h3>
+        <div className='flex gap-2'>
+          <div className='h-15 w-15 bg-purple-300 rounded-xl px-2 py-1'></div>
+          <div className='h-15 w-15 bg-yellow-400 rounded-xl px-2 py-1'></div>
+          <div className='h-15 w-15 bg-red-400 rounded-xl px-2 py-1'></div>
+        </div>
+        
+      </div>
+      
+    </div>
   )
 }
 
